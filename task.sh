20924fbe6c3c5fa1dbd861f39d055e9221546a59
Oleksii Stepanyk <150719321+Oleksii-Stepanyk@users.noreply.github.com>
2024-11-25 09:37:37 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -7,23 +7,23 @@ add() {
     read number
     echo "Enter email"
     read email
-    echo "$name $number $email" >> $HOME/homework/book.txt
+    echo "$name $number $email" >> book.txt
     echo "Contact added."
 }
 
 search() {
     echo "Enter data"
     read data
-    grep "$data" $HOME/homework/book.txt || echo "No contact found."
+    grep "$data" book.txt || echo "No contact found."
 }
 
 remove() {
     echo "Enter data"
     read data
-    grep -v "$data" $HOME/homework/book.txt > tmp.txt && mv tmp.txt $HOME/homework/book.txt
+    grep -v "$data" book.txt > tmp.txt && mv tmp.txt book.txt
 }
 
-touch $HOME/homework/book.txt
+sudo touch book.txt
 if [[ "$1" == "add" ]]; then add
 elif [[ "$1" == "search" ]]; then search
 elif [[ "$1" == "remove" ]]; then remove
